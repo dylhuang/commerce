2d89d049ff1aecf1b5d7326322e6af729f86990c
mzhoulei <mzhoulei@126.com>
2024-08-09 09:23:47 +0800

    登录模块： 1、登录接口

--- src/main/java/com/group/consult/commerce/controller/sys/LoginController.java
+++ src/main/java/com/group/consult/commerce/controller/sys/LoginController.java
@@ -3,7 +3,9 @@ package com.group.consult.commerce.controller.sys;
 import com.group.consult.commerce.model.ApiResult;
 import com.group.consult.commerce.model.dto.LoginDTO;
 import com.group.consult.commerce.model.vo.LoginVO;
+import com.group.consult.commerce.service.ISysLoginDomainService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/sys")
 public class LoginController {
 
+    @Autowired
+    private ISysLoginDomainService loginDomainService;
+
     /**
      * 登录
      * @param loginDTO
@@ -27,7 +32,10 @@ public class LoginController {
      */
     @PostMapping("/login")
     public ApiResult<LoginVO> login(@RequestBody LoginDTO loginDTO) {
-        return null;
+        String token = loginDomainService.login(loginDTO);
+        LoginVO loginVO = new LoginVO();
+        loginVO.setToken(token);
+        return ApiResult.success(loginVO);
     }
 
 }
